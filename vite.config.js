45d7913901d14acd5a888a67a16e4ab3ae8570e2
Jatin-1111 <off.jatin1111@gmail.com>
2024-12-10 19:42:36 +0530

    REACT

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/edusphere-react/', // Replace with your repository name
+  plugins: [react()],
 })
